5b76afc2d8d9a94397bd4d8f2cba6becb368929f
Brian <mahuibrian14@gmail.com>
2023-05-26 13:35:02 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
   # your code here
+  def initialize (attributes={})
+    attributes.each do |attribute,value|
+      self.class.send(:attr_accessor, attribute)
+      send("#{attribute}=", value)
+    end
+  end
+    
 end
